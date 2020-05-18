881a8e2bc07249f719227460ae6b0d9d1d73e0d8
Maura Kelly <maurak87@gmail.com>
2020-05-18 19:03:22 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,29 @@
 def addition(num1, num2)
-
+  num1 + num2 = result
+  result
 end
 
 def subtraction(num1, num2)
-
+  num1 - num2 = result
+  result
 end
 
 def division(num1, num2)
-
+  num1 / num2 = result
+  result
 end
 
 def multiplication(num1, num2)
-
+  num1*num2 = result
+  result
 end
 
 def modulo(num1, num2)
-
+  num1 / num2 = %
+  %
 end
 
 def square_root(num)
+  
 
 end
